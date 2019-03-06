c67b0aecac10883e39ecc70790a09f5a5fdbc860
Johnnynater <42873923+johnnynater@users.noreply.github.com>
2019-03-06 14:33:44 +0100

    Made simple GUI

---
+++ src/main.py
@@ -0,0 +1,13 @@
+# Simple GUI
+
+from tkinter import *
+
+# Create a window
+root = Tk()
+
+# Modify root window
+root.title("Name of our tool")
+root.geometry("400x400")
+
+# Kick off main-event loop
+root.mainloop()
