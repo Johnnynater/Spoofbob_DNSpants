3fc67d3e44d28589e7a226e46ef1a1906b1842d3
Johnnynater <42873923+johnnynater@users.noreply.github.com>
2019-03-06 15:33:08 +0100

    Made simple GUI with a button, entry box, and text

--- src/main.py
+++ src/main.py
@@ -9,5 +9,18 @@ root = Tk()
 root.title("Name of our tool")
 root.geometry("400x400")
 
+app = Frame(root)
+app.grid()
+
+label = Label(app, text="Some text")
+label.grid()
+
+button1 = Button(app, text="a button")
+button1.grid()
+
+button2 = Button(app)
+button2.grid()
+button2.configure(text="another button")
+
 # Kick off main-event loop
 root.mainloop()
---
+++ src/userInterface.py
@@ -0,0 +1,67 @@
+from tkinter import *
+
+
+class Application(Frame):
+    """" A GUI Application with buttons """
+
+    def __init__(self, master):
+        """" Initializes the frame """
+        Frame.__init__(self, master)
+        self.grid()
+        self.button_clicks = 0
+        self.create_widgets()
+
+    def create_widgets(self):
+        """" Create button and text """
+        # Create a label
+        self.instruction = Label(self)
+        self.instruction["text"] = "Enter your IP Address"
+        self.instruction.grid(row=0, column=0, columnspan=2, sticky=W)
+
+        # Create an entry
+        self.pswd = Entry(self)
+        self.pswd.grid(row=1, column=1, sticky=W)
+
+        # Create a submit button
+        self.submit_button = Button(self)
+        self.submit_button["text"] = "Submit"
+        self.submit_button["command"] = self.reveal
+        self.submit_button.grid(row=2, column=0, sticky=W)
+
+        # Create text
+        self.text = Text(self, width=35, height=5, wrap=WORD)
+        self.text.grid(row=3, column=0, columnspan=2, sticky=W)
+
+        # Create a counter button
+        self.button = Button(self)
+        self.button["text"] = "Total clicks: 0"
+        self.button["command"] = self.update_counter
+        self.button.grid()
+
+    def update_counter(self):
+        """" Update the counter and display it on the button """
+        self.button_clicks += 1
+        self.button["text"] = "Total clicks: " + str(self.button_clicks)
+
+    def reveal(self):
+        """ Display message based on password typed """
+        content = self.pswd.get()
+
+        if (content == "password"):
+            message = "Good job fam"
+
+        else:
+            message = "Access denied"
+
+        self.text.delete(0.0, END)
+        self.text.insert(0.0, message)
+
+
+
+root = Tk()
+root.title("Three buttons")
+root.geometry("300x300")
+
+app = Application(root)
+
+root.mainloop()
